62916298265918f58f8749cda472a22d55e1a417
kailua <kailua@project2by2.jp>
2026-09-19 22:39:14 +0900

    SX3: Initial commit

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),SX3)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,b0563729ce8919b25ac704bd928043c07790d9db)
+$(call add-radio-file-sha1-checked,radio/gz.img,aefd4e0011d3602c7a94e839a480ac7ea63a60ed)
+$(call add-radio-file-sha1-checked,radio/lk.img,6ae693a1c8a4348e9e254d3aee4ff71856b74bed)
+$(call add-radio-file-sha1-checked,radio/logo.img,a0910ca66b66071d61be71eda2fc7e48203eafd2)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,523453be07b1990845692d061fbae21c55d553d6)
+$(call add-radio-file-sha1-checked,radio/md1img.img,1ea62be8ddbc58e7489382dbbc3f5062a93e39cd)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,490117f9f84cd4b61c162013047044eee54255cf)
+$(call add-radio-file-sha1-checked,radio/preloader.img,c3f1e827a0ae3ee340306ade05c6c33be176c976)
+$(call add-radio-file-sha1-checked,radio/scp.img,44461031f7c4094a7b929dc9a2d330823bbfaa71)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,17703e4ea8ef6558efe4537814a3129505d82b42)
+$(call add-radio-file-sha1-checked,radio/sspm.img,6c3b0b222c3f47c8d01dd5eebaa1b320bbd5bf59)
+$(call add-radio-file-sha1-checked,radio/tee.img,672c01286741fdab82bab7e92cc5c6413a7284ef)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,17 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    dpm \
+    gz \
+    lk \
+    logo \
+    mcupm \
+    md1img \
+    pi_img \
+    preloader \
+    scp \
+    spmfw \
+    sspm \
+    tee
